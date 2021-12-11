33e070114dc42b680c6b12631db63c19627ee14d
AndriiPyatnitskyi <pyatnitskiy.a@gmail.com>
2021-12-11 21:45:51 +0200

    change permit roles

--- src/routes/accounts.ts
+++ src/routes/accounts.ts
@@ -7,15 +7,14 @@ import accountSchema from "../validator/validator";
 const permit = require("../middleware/auth");
 const accountRouter = express.Router();
 
-accountRouter.get('/api/accounts', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.getAccounts);
-accountRouter.get('/api/accounts/:id', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.getAccountById);
-accountRouter.post('/api/accounts', validator(accountSchema), permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.createAccount);
-accountRouter.put('/api/accounts/:id', validator(accountSchema), permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.updateAccount);
+accountRouter.get('/api/accounts', permit([Role.ADMIN]), controller.getAccounts);
+accountRouter.get('/api/accounts/:id', permit([Role.USER, Role.ADMIN]), controller.getAccountById);
+accountRouter.post('/api/accounts', validator(accountSchema), permit([ Role.USER, Role.ADMIN]), controller.createAccount);
+accountRouter.put('/api/accounts/:id', validator(accountSchema), permit([ Role.USER, Role.ADMIN]), controller.updateAccount);
 accountRouter.delete('/api/accounts/:id', permit([Role.ADMIN]), controller.deleteAccount);
-accountRouter.get('/api/accounts/:id/tokens', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.getAccountTokensByAccountId);
-accountRouter.post('/api/accounts/:id/tokens', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.createAccountToken);
-accountRouter.put('/api/accounts/:id/tokens', permit([Role.ANONYMOUS, Role.USER, Role.ADMIN]), controller.updateAccountToken);
+accountRouter.get('/api/accounts/:id/tokens', permit([ Role.USER, Role.ADMIN]), controller.getAccountTokensByAccountId);
+accountRouter.post('/api/accounts/:id/tokens', permit([ Role.USER, Role.ADMIN]), controller.createAccountToken);
+accountRouter.put('/api/accounts/:id/tokens', permit([ Role.USER, Role.ADMIN]), controller.updateAccountToken);
 accountRouter.delete('/api/accounts/:id/tokens', permit([Role.ADMIN]), controller.deleteAccountToken);
-
 
 export default accountRouter;
